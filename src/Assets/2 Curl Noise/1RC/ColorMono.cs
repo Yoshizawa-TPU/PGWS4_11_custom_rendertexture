e8b8b3536cfe550fc13261d64bfe6745f25581ae
totoフォリオ <146707740+Yoshizawa-TPU@users.noreply.github.com>
2026-01-14 16:22:52 +0900

    試行錯誤

---
+++ src/Assets/2 Curl Noise/1RC/ColorMono.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UIElements;
+
+public class ColorMono : MonoBehaviour
+{
+    [SerializeField] CustomRenderTexture texField = default!;
+    [SerializeField] CustomRenderTexture texColor = default!;
+    [SerializeField] Material m_Material = default!;
+    float intensity = 0.0f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        texField.Initialize();
+        texColor.Initialize();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_Material.SetFloat("_intensity", intensity);
+        intensity = 0.0f;
+    }
+
+    public void OnDrag(BaseEventData e)
+    {
+        RectTransform rt = transform as RectTransform;
+        Vector2 lp;
+        if(RectTransformUtility.ScreenPointToLocalPointInRectangle(rt,(e as PointerEventData).position,null,out lp))
+        {
+            Vector2 cood = lp / rt.rect.size + rt.pivot;
+            m_Material.SetVector("_Position", new Vector4(cood.x, cood.y, 0, 0));
+            intensity = 0.0001f;
+        }
+    }
+}
